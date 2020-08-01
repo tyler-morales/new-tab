1ad451c0806c0441f6954914efe45a453791143c
Tyler Morales <moratyle@gmail.com>
2020-07-31 22:54:47 -0500

    ✨ Implement name and location feature for images

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,12 @@
   <div id="app" :style="imageStyleObject">
     <Clock />
     <QuoteBox />
+    <div class="unsplash-img">
+      <span class="unsplash-img--location">{{ locationName }}</span>
+      <span class="unsplash--img--photog"
+        ><a v-bind:href="url">{{ name }}</a></span
+      >
+    </div>
   </div>
 </template>
 
@@ -18,19 +24,25 @@ export default {
   },
   data() {
     return {
-      image: ''
+      image: '',
+      locationName: '',
+      name: '',
+      url: ''
     }
   },
   methods: {
     renderImage: function() {
       axios
         .get(
-          // 'https://api.unsplash.com/photos/random/?client_id=8rzeKbxcD7bHreUEfXY7wpS4VfYNHg1NTMBqucML0iw'
+          'https://api.unsplash.com/photos/random/?client_id=8rzeKbxcD7bHreUEfXY7wpS4VfYNHg1NTMBqucML0iw&query=nature&orientation=landscape'
         )
         .then(response => {
           console.log([response.data, response.data.location.title])
           console.log('🎉 Image loaded succesfully')
           this.image = response.data.urls.regular
+          this.locationName = response.data.location.title
+          this.name = response.data.user.name
+          this.url = response.data.user.links.html
         })
     }
   },
@@ -68,5 +80,20 @@ export default {
   padding: 75px;
   display: grid;
   grid-template-rows: repeat(12, 1fr);
+  grid-template-columns: repeat(2, 1fr);
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  .unsplash-img {
+    grid-column: 2;
+    grid-row: -1;
+    text-align: right;
+    display: flex;
+    flex-direction: column;
+    font-size: 14px;
+  }
 }
 </style>
